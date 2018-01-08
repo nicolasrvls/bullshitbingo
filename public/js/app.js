d6366baaf29fba04a673a5960fa9fdcc79e2bcd2
nicolasrvls <32567292+nicolasrvls@users.noreply.github.com>
2018-01-08 10:00:39 +0100

    New branch

--- public/js/app.js
+++ public/js/app.js
@@ -10,12 +10,35 @@ const games = client.service('games');
 
 // Log when anyone creates a new message in real-time!
 games.on('created', game =>
-  alert(`New game with id ${game.game_id}`)
+  alert(`New game with id ${game._id}`)
 );
 
 // Create a test message
-games.create({
-  game_id: 'abc'
-});
+createGame("test");
 
 games.find().then(results => console.log('Current games are', results));
+
+
+function createGame(g_name) {
+  games.create({
+    game_name: g_name,
+    createdAt: new Date(),
+    words: generateWords(),
+    players: []
+  });
+}
+
+function generateWords() {
+  var r = [];
+  var t = cartes;
+  for (var i = 0; i < 9; i++) {
+    k = Math.floor(Math.random() * t.length);
+    r.push(
+      {
+        "word" : t.splice(k,1)[0],
+        "marked": false
+      }
+    );
+  }
+  return r;
+}
